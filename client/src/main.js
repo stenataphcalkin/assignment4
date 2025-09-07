15941332cbf64f78a00e32b4104d985030375b40
Steph <steph@polyhydral.co.uk>
2025-09-07 11:06:26 +0100

    11am milestone. Expanded form, testing connection with render.

--- client/src/main.js
+++ client/src/main.js
@@ -1,25 +1,27 @@
 //TODO: Collect user's data from the form and send it to the server
 
 //select the form from the DOM
-const staffForm = document.getElementById("staff-form");
+const rateMyAdventurer = document.getElementById("RMA-form");
 
 //add a submit event to the form
-staffForm.addEventListener("submit", handleSubmit);
+rateMyAdventurer.addEventListener("submit", handleSubmit);
 
 //handle the submit event
 function handleSubmit(event) {
   //prevent the data from going to the url
   event.preventDefault();
+
   //create a new template object using my form inputs
-  const formData = new FormData(staffForm); //this is the box to store our input data
+  const formData = new FormData(rateMyAdventurer); //this is the box to store our input data
   //fill the input data in to the template object
   const formValues = Object.fromEntries(formData);
   console.log(formValues);
+
   //send the data to the server
   //- we have a POST route called add-staff in the server, ready to receive our formValues
   //- connect client with specific server route --> fetch
   //- add the formValues to the fetch request --> body
-  fetch("http://localhost:8080/add-staff", {
+  fetch("https://assignment4-wqkw.onrender.com/khasrae", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -27,3 +29,5 @@ function handleSubmit(event) {
     body: JSON.stringify({ formValues }),
   });
 }
+
+//TODO: Need to look at retrieving reviews and appending the new one on submission ^
